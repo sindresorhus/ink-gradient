7d8bf3176c9437ae52e45b20ba6f658946b514ce
Vadim Demedes <vdemedes@gmail.com>
2019-03-04 00:15:43 -0800

    Use Ink 2.0.0

--- test.js
+++ test.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import {serial as test} from 'ava';
-import {renderToString} from 'ink';
+import {render} from 'ink';
 import clearModule from 'clear-module';
 import stripAnsi from 'strip-ansi';
+
+// Fake process.stdout
+class Stream {
+	constructor() {
+		this.output = '';
+		this.columns = 100;
+	}
+
+	write(str) {
+		this.output = str;
+	}
+
+	get() {
+		return this.output;
+	}
+}
+
+const renderToString = node => {
+	const stream = new Stream();
+
+	render(node, {
+		stdout: stream,
+		debug: true
+	});
+
+	return stream.get();
+};
 
 test('render', t => {
 	// Render color here too when https://github.com/chalk/supports-color/issues/78 is fixed
